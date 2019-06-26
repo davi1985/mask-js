81fee8b96b9f6bf35df3eb8b39fcaf57c0ab3181
Davi Silva <davisilvaphoto@gmail.com>
2019-06-26 11:42:49 -0300

    vanilla js masks

--- app.js
+++ app.js
@@ -1,54 +1,54 @@
 //vanilla js masks
 
 const masks = {
-    cpf(value) {
-        return value
-            .replace(/\D/g, "")
-            .replace(/(\d{3})(\d)/, "$1.$2")
-            .replace(/(\d{3})(\d)/, "$1.$2")
-            .replace(/(\d{3})(\d{1,2})/, "$1-$2")
-            .replace(/(-\d{2})\d+?$/, "$1");
-    },
-    cnpj(value) {
-        return value
-            .replace(/\D/g, "")
-            .replace(/(\d{2})(\d)/, "$1.$2")
-            .replace(/(\d{3})(\d)/, "$1.$2")
-            .replace(/(\d{3})(\d)/, "$1/$2")
-            .replace(/(\d{4})(\d)/, "$1-$2")
-            .replace(/(-\d{2})\d+?$/, "$1");
-    },
-    phone(value) {
-        return value
-            .replace(/\D/g, "")
-            .replace(/(\d{2})(\d)/, "($1) $2")
-            .replace(/(\d{4})(\d)/, "$1-$2")
-            .replace(/(\d)(\d{3})-(\d)(\d{4})/, "$1 $2$3-$4")
-            .replace(/(\-\d{4})\d+?$/, "$1");
-    },
-    cep(value) {
-        return value
-            .replace(/\D/g, "")
-            .replace(/(\d{5})(\d{3})/, "$1-$2")
-            .replace(/(-\d{3})\d+?$/, "$1");
-    },
-    pis(value) {
-        return value
-            .replace(/\D/g, "")
-            .replace(/(\d{3})(\d)/, "$1.$2")
-            .replace(/(\d{5})(\d)/, "$1.$2")
-            .replace(/(\d{5}\.)(\d{2})(\d)/, "$1$2-$3")
-            .replace(/(-\d)\d+?$/, "$1");
-    }
+  cpf(value) {
+    return value
+      .replace(/\D/g, "")
+      .replace(/(\d{3})(\d)/, "$1.$2")
+      .replace(/(\d{3})(\d)/, "$1.$2")
+      .replace(/(\d{3})(\d{1,2})/, "$1-$2")
+      .replace(/(-\d{2})\d+?$/, "$1");
+  },
+  cnpj(value) {
+    return value
+      .replace(/\D/g, "")
+      .replace(/(\d{2})(\d)/, "$1.$2")
+      .replace(/(\d{3})(\d)/, "$1.$2")
+      .replace(/(\d{3})(\d)/, "$1/$2")
+      .replace(/(\d{4})(\d)/, "$1-$2")
+      .replace(/(-\d{2})\d+?$/, "$1");
+  },
+  phone(value) {
+    return value
+      .replace(/\D/g, "")
+      .replace(/(\d{2})(\d)/, "($1) $2")
+      .replace(/(\d{4})(\d)/, "$1-$2")
+      .replace(/(\d)(\d{3})-(\d)(\d{4})/, "$1 $2$3-$4")
+      .replace(/(\-\d{4})\d+?$/, "$1");
+  },
+  cep(value) {
+    return value
+      .replace(/\D/g, "")
+      .replace(/(\d{5})(\d{3})/, "$1-$2")
+      .replace(/(-\d{3})\d+?$/, "$1");
+  },
+  pis(value) {
+    return value
+      .replace(/\D/g, "")
+      .replace(/(\d{3})(\d)/, "$1.$2")
+      .replace(/(\d{5})(\d)/, "$1.$2")
+      .replace(/(\d{5}\.)(\d{2})(\d)/, "$1$2-$3")
+      .replace(/(-\d)\d+?$/, "$1");
+  }
 };
 
 document.querySelectorAll("input").forEach($input => {
-    const field = $input.dataset.js;
-    $input.addEventListener(
-        "input",
-        e => {
-            e.target.value = masks[field](e.target.value);
-        },
-        false
-    );
+  const field = $input.dataset.js;
+  $input.addEventListener(
+    "input",
+    e => {
+      e.target.value = masks[field](e.target.value);
+    },
+    false
+  );
 });
